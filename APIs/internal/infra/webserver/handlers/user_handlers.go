96998fb7ef4c0f782bd0f5fb938203169e79e3c2
valdir-alves3000 <valdiralves3000@gmail.com>
2024-08-26 13:38:48 -0300

    feat(handler): add GetJWT method to UserHandler for JWT generation on user authentication

--- APIs/internal/infra/webserver/handlers/user_handlers.go
+++ APIs/internal/infra/webserver/handlers/user_handlers.go
@@ -3,20 +3,58 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
+	"github.com/go-chi/jwtauth"
 	"github.com/valdir-alves3000/postgraduate-challenges-go-expert/APIs/internal/dto"
 	"github.com/valdir-alves3000/postgraduate-challenges-go-expert/APIs/internal/entity"
 	"github.com/valdir-alves3000/postgraduate-challenges-go-expert/APIs/internal/infra/database"
 )
 
 type UserHandler struct {
-	UserDB database.UserInterface
+	UserDB        database.UserInterface
+	Jwt           *jwtauth.JWTAuth
+	JwtExperiesIn int
 }
 
-func NewUserHandler(db database.UserInterface) *UserHandler {
+func NewUserHandler(db database.UserInterface, jwt *jwtauth.JWTAuth, jwtExperiesIn int) *UserHandler {
 	return &UserHandler{
-		UserDB: db,
+		UserDB:        db,
+		Jwt:           jwt,
+		JwtExperiesIn: jwtExperiesIn,
 	}
+}
+
+func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	_, tokenString, _ := h.Jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(h.JwtExperiesIn)).Unix(),
+	})
+
+	accessToken := struct {
+		AccessToken string `json:"accessToken"`
+	}{
+		AccessToken: tokenString,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
 }
 
 func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
