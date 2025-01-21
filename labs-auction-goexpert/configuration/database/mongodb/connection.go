230ac9e73ac28c993bf825996d665cb04c48c39a
valdir-alves3000 <valdiralves3000@gmail.com>
2025-01-21 10:13:07 -0300

    feat: add configuration files for MongoDB, logger, and REST errors

---
+++ labs-auction-goexpert/configuration/database/mongodb/connection.go
@@ -0,0 +1,36 @@
+package mongodb
+
+import (
+	"context"
+	"os"
+
+	"github.com/valdir-alves3000/postgraduate-challenges-go-expert/labs-auction-goexpert/configuration/logger"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	MONGODB_URL = "MONGODB_URL"
+	MONGODB_DB  = "MONGODB_DB"
+)
+
+func NewMongoDBConnection(ctx context.Context) (*mongo.Database, error) {
+	mongoURL := os.Getenv(MONGODB_URL)
+	mongoDatabase := os.Getenv(MONGODB_DB)
+	logger.Info("Error trying to connect to mongodb database")
+
+	client, err := mongo.Connect(
+		ctx, options.Client().ApplyURI(mongoURL))
+	if err != nil {
+		logger.Error("Error trying to connect to mongodb database", err)
+		return nil, err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		logger.Error("Error trying to ping mongodb database", err)
+		return nil, err
+	}
+
+	return client.Database(mongoDatabase), nil
+}
