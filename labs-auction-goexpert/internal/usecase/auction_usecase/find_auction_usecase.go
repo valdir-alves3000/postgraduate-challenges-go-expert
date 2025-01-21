a7e6fac3ca8fcac2b7ce3f38393208eac855a67e
valdir-alves3000 <valdiralves3000@gmail.com>
2025-01-21 10:21:53 -0300

    feat: add use cases for creating and finding auctions

---
+++ labs-auction-goexpert/internal/usecase/auction_usecase/find_auction_usecase.go
@@ -0,0 +1,98 @@
+package auction_usecase
+
+import (
+	"context"
+
+	"github.com/valdir-alves3000/postgraduate-challenges-go-expert/labs-auction-goexpert/configuration/logger"
+	"github.com/valdir-alves3000/postgraduate-challenges-go-expert/labs-auction-goexpert/internal/entity/auction_entity"
+	"github.com/valdir-alves3000/postgraduate-challenges-go-expert/labs-auction-goexpert/internal/internal_error"
+	"github.com/valdir-alves3000/postgraduate-challenges-go-expert/labs-auction-goexpert/internal/usecase/bid_usecase"
+)
+
+func (au *AuctionUseCase) FindAuctionById(
+	ctx context.Context, id string) (*AuctionOutputDTO, *internal_error.InternalError) {
+	auctionEntity, err := au.auctionRepositoryInterface.FindAuctionById(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &AuctionOutputDTO{
+		Id:          auctionEntity.Id,
+		ProductName: auctionEntity.ProductName,
+		Category:    auctionEntity.Category,
+		Description: auctionEntity.Description,
+		Condition:   ProductCondition(auctionEntity.Condition),
+		Status:      AuctionStatus(auctionEntity.Status),
+		Timestamp:   auctionEntity.Timestamp,
+		ExpiresAt:   auctionEntity.ExpiresAt,
+	}, nil
+}
+
+func (au *AuctionUseCase) FindAuctions(
+	ctx context.Context,
+	status AuctionStatus,
+	category, productName string) ([]AuctionOutputDTO, *internal_error.InternalError) {
+	auctionEntities, err := au.auctionRepositoryInterface.FindAuctions(
+		ctx, auction_entity.AuctionStatus(status), category, productName)
+	if err != nil {
+		return nil, err
+	}
+
+	var auctionOutputs []AuctionOutputDTO
+	for _, value := range auctionEntities {
+		auctionOutputs = append(auctionOutputs, AuctionOutputDTO{
+			Id:          value.Id,
+			ProductName: value.ProductName,
+			Category:    value.Category,
+			Description: value.Description,
+			Condition:   ProductCondition(value.Condition),
+			Status:      AuctionStatus(value.Status),
+			Timestamp:   value.Timestamp,
+			ExpiresAt:   value.ExpiresAt,
+		})
+	}
+
+	return auctionOutputs, nil
+}
+
+func (au *AuctionUseCase) FindWinningBidByAuctionId(
+	ctx context.Context,
+	auctionId string) (*WinningInfoOutputDTO, *internal_error.InternalError) {
+	auction, err := au.auctionRepositoryInterface.FindAuctionById(ctx, auctionId)
+	if err != nil {
+		return nil, err
+	}
+
+	auctionOutputDTO := AuctionOutputDTO{
+		Id:          auction.Id,
+		ProductName: auction.ProductName,
+		Category:    auction.Category,
+		Description: auction.Description,
+		Condition:   ProductCondition(auction.Condition),
+		Status:      AuctionStatus(auction.Status),
+		Timestamp:   auction.Timestamp,
+		ExpiresAt:   auction.ExpiresAt,
+	}
+
+	bidWinning, err := au.bidRepositoryInterface.FindWinningBidByAuctionId(ctx, auction.Id)
+	if err != nil {
+		logger.Error("", err)
+		return &WinningInfoOutputDTO{
+			Auction: auctionOutputDTO,
+			Bid:     nil,
+		}, nil
+	}
+
+	bidOutputDTO := &bid_usecase.BidOutputDTO{
+		Id:        bidWinning.Id,
+		UserId:    bidWinning.UserId,
+		AuctionId: bidWinning.AuctionId,
+		Amount:    bidWinning.Amount,
+		Timestamp: bidWinning.Timestamp,
+	}
+
+	return &WinningInfoOutputDTO{
+		Auction: auctionOutputDTO,
+		Bid:     bidOutputDTO,
+	}, nil
+}
